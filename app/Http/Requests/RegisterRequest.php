61375b6a907eaf41430269faadc7be1fc783ead6
MateGellertVarga <vargamategellert@gmail.com>
2025-04-16 18:00:42 +0200

    password regex fix

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -17,7 +17,12 @@ class RegisterRequest extends FormRequest
             'name'         => 'required|string',
             'email'        => 'required|email|unique:users,email',
             'phone_number' => 'required|phone:SK,HU,RO,PL,CZ,AT,DE,INTERNATIONAL',
-            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"\'<>,.?\/~`\\|-])[A-Za-z\d!@#$%^&*()_+={}\[\]:;"\'<>,.?\/~`\\|-]{8,}$/',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*\(\)_\+\-=\[\]\{\};:"\\|,.<>\/\?])[A-Za-z\d!@#\$%\^&\*\(\)_\+\-=\[\]\{\};:"\\|,.<>\/\?]{8,}$/'
+            ],
             'role'         => 'required|in:user,admin'
         ];
     }
