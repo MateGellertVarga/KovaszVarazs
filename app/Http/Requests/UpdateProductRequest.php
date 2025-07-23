aebe1f4f0c7b5eb27c60543a8e8f14fda743df51
MateGellertVarga <vargamategellert@gmail.com>
2025-07-23 15:37:56 +0200

    product request fix

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -14,28 +14,23 @@ class UpdateProductRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
-            'price' => 'required|numeric',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
-            'category' => 'required|string',
-            'is_used' => 'required|boolean',
+            'name' => 'sometimes|string',
+            'price' => 'sometimes|numeric',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
+            'category' => 'sometimes|string',
+            'is_used' => 'sometimes|boolean',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'name.required' => 'Név hiányzik',
             'name.string'   => 'Név formátuma nem megfelelő',
-            'price.required' => 'Ár hiányzik',
             'price.numeric'  => 'Ár formátuma nem megfelelő',
-            'image.required' => 'Kép hiányzik',
             'image.image'    => 'Kép formátuma nem megfelelő',
             'image.mimes'    => 'Kép formátuma nem megfelelő',
             'image.max'      => 'Kép mérete túl nagy',
-            'category.required' => 'Kategória hiányzik',
             'category.string'   => 'Kategória formátuma nem megfelelő',
-            'is_used.required' => 'Használati állapot hiányzik',
             'is_used.boolean'  => 'Használati állapot formátuma nem megfelelő',
         ];
     }
