ebe84d64e1ec63e5bae2379854845a09baa61aac
MateGellertVarga <vargamategellert@gmail.com>
2025-08-27 11:35:49 +0200

    Only block registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,8 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //$middleware->append(StartSession::class);
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
-        ]);
-        $middleware->alias([
             'cookie_or_bearer' => \App\Http\Middleware\CookieOrBearerAuth::class,
         ]);
 
